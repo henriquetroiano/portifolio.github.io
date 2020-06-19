f9cd29a3f89a8b4bf3d79a143d435ab7bb2365ae
Henrique Troiano <henrique.troiano@omel.com.br>
2020-06-19 13:22:21 -0300

    layout mobile

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import personalData from "./scripts/personalData.js"
+import personaldatamobile from "./scripts/personalDatamobile.js"
 import skills from "./scripts/skills.js"
 import navigation from "./scripts/navigation.js"
 import navigationMobile from "./scripts/navigationMobile.js"
 
 
 personalData();
+personaldatamobile();
 skills();
 navigation();
 navigationMobile();
