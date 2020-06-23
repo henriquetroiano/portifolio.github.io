eadafdd2f54f97377025a1a891c518a8e2c87519
Henrique Troiano <henrique.troiano@omel.com.br>
2020-06-23 17:12:29 -0300

    mobile layout

--- scripts/navigationMobile.js
+++ scripts/navigationMobile.js
@@ -3,6 +3,7 @@ export default function navigationmobile() {
     function switchMenu() {
         const topMenu = document.querySelector('.topMenu');
         topMenu.classList.toggle('active')
+        
     }
 
     const menuButton = document.querySelector('.topMenu #menuButton');
