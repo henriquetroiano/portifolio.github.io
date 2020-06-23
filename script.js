849d66b6f7d4580c933a2c1939996196f65f28b2
Henrique Troiano <henrique.troiano@omel.com.br>
2020-06-23 16:09:38 -0300

    mobile leyout

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import personalData from "./scripts/personalData.js"
-import personaldatamobile from "./scripts/personalDatamobile.js"
 import skills from "./scripts/skills.js"
 import navigation from "./scripts/navigation.js"
-import navigationMobile from "./scripts/navigationMobile.js"
+
 
 
 personalData();
