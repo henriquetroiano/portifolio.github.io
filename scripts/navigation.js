30472d0f39c7885d257b47b5b161a18280d3b90a
Henrique Troiano <henrique.troiano@omel.com.br>
2020-06-25 14:31:49 -0300

    layout mobile

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -8,11 +8,26 @@ export default function navigation() {
 
         button.addEventListener('click', function(event) {
             event.preventDefault();
+            const home1 = document.querySelector('.personalData');
+            const home2 = document.querySelector('.about');
+            const skills = document.querySelector('.skills');
             const allbtns = document.querySelectorAll('.menu a')
             allbtns.forEach((btn) => {
                 btn.classList.remove('active');
             })
             button.classList.add('active')
+
+            if(event.target.innerText == "HOME") {
+                skills.classList.toggle('none')
+                
+            } else if(event.target.innerText == "SKILLS") {
+                home1.classList.toggle('none');
+                home2.classList.toggle('none');
+            }
+
+            console.log(event.target.innerText == 'SKILLS')
         })
     })
+
+
 }
--- scripts/navigationMobile.js
+++ scripts/navigationMobile.js
@@ -3,8 +3,12 @@ export default function navigationmobile() {
     function switchMenu() {
         const topMenu = document.querySelector('.topMenu');
         topMenu.classList.toggle('active')
+        
     }
 
     const menuButton = document.querySelector('.topMenu #menuButton');
     menuButton.addEventListener('click', switchMenu)
+
+
+    
 }
