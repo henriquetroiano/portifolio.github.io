ca8147e009a1ccb1ba544bf1136fef33852fa60c
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-27 20:18:54 -0300

    configuracao de layout responsivo

--- scripts/personalData.js
+++ scripts/personalData.js
@@ -5,11 +5,11 @@ export default function personalData() {
     span.addEventListener('click', function() { 
         const active1 = document.querySelector('.personalData')
         const active2 = document.querySelector('.personalData #span')
-        const active3 = document.querySelector('.personalData #resume')
+        // const active3 = document.querySelector('.personalData #resume')
     
         active1.classList.toggle('active')
         active2.classList.toggle('active')
-        active3.classList.toggle('active')
+        // active3.classList.toggle('active')
     })
 
     const arrowbtn = document.querySelector('#arrow span')
