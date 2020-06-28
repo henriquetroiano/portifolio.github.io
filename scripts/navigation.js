11a76123cee4c56009e17fcc31cd21ff48b8669f
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-28 17:01:47 -0300

    layout desktop config

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -18,6 +18,9 @@ export default function navigation() {
         const allbtns = document.querySelectorAll(".menu a");
         const arrow = document.querySelector('#arrow span')
         
+        const home1 = document.querySelector('.personalData');
+        const home2 = document.querySelector('.about');
+
         allbtns.forEach((button) => {
             button.classList.remove('active');
             arrow.classList.add('none')
@@ -25,36 +28,45 @@ export default function navigation() {
             const divs = document.querySelectorAll('.display > div');
             divs.forEach((div) => {
                 div.classList.add('none', 'nonedesktop')
+                home1.classList.remove('nonedesktop');
+                home2.classList.remove('nonedesktop');
+
             })   
         })
         
         if(event.target.innerText == "HOME") {
             event.target.classList.add('active');
             arrow.classList.remove('none')
-            const home1 = document.querySelector('.personalData');
             home1.classList.remove('none', 'nonedesktop');
-            const home2 = document.querySelector('.about');
             home2.classList.remove('none', 'nonedesktop');
 
         } else if(event.target.innerText == 'SKILLS') {
             event.target.classList.add('active')
             const skills = document.querySelector('.skills');
             skills.classList.remove('none', 'nonedesktop');
+            home2.classList.add('nonedesktop');
+
 
         } else if(event.target.innerText == 'PROJETOS') {
             event.target.classList.add('active')
             const projects = document.querySelector('.projects');
             projects.classList.remove('none', 'nonedesktop');
+            home2.classList.add('nonedesktop');
+
 
         } else if(event.target.innerText == 'CURSOS') {
             event.target.classList.add('active')
             const courses = document.querySelector('.courses');
             courses.classList.remove('none', 'nonedesktop');
+            home2.classList.add('nonedesktop');
+
 
         } else if(event.target.innerText == 'CONTATO') {
             event.target.classList.add('active')
             const contacts = document.querySelector('.contact');
-            contacts.classList.remove('none', 'nonedesktop')
+            contacts.classList.remove('none', 'nonedesktop');
+            home2.classList.add('nonedesktop');
+
         }
     }
 
